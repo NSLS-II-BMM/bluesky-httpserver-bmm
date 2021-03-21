e9d666e11afb5068b406093bcd3f9c90e0cc032d
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2021-03-21 10:42:13 -0400

    STY: apply 'black .' and 'flake'

--- bluesky_httpserver_bmm/__init__.py
+++ bluesky_httpserver_bmm/__init__.py
@@ -1,5 +1,6 @@
 from .spreadsheets import spreadsheet_to_plan_list  # noqa: F401
 
 from ._version import get_versions
-__version__ = get_versions()['version']
+
+__version__ = get_versions()["version"]
 del get_versions
